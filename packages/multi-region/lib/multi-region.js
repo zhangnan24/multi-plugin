fcc96d730e058a8a7f42e0a402401c77617e9575
derik.zhang <derik.zhang@shopee.com>
2021-10-26 15:25:03 +0800

    fix: 完善了文档和报错提示

--- packages/multi-region/lib/multi-region.js
+++ packages/multi-region/lib/multi-region.js
@@ -1,11 +1,24 @@
 "use strict";
 
+/**
+ * 初始化Plugin
+ * @param {Object} options 传入的配置对象，请提供键名regionVariable
+ */
 function MultiRegionPlugin(options) {
     this.options = options
 }
 
 MultiRegionPlugin.prototype.apply = function (compiler) {
   const opts = this.options;
+
+  if (!opts) {
+    throw new Error("missing the parameter 'options' when instantiate multi-region plugin")
+  }
+
+  if (!opts.regionVariable) {
+    throw new Error("missing the parameter 'regionVariable' in options")
+  }
+
   compiler.plugin("entryOption", function () {
     // 不同情景变量
     const region = process.env[opts.regionVariable];
